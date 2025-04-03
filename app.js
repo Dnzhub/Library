a4a0f69dd698e59d9312324a9b9f052b43c72713
Dnzhub <dnz.ylmz1@outlook.com>
2025-04-03 17:49:59 +0300

    Implement add and remove book functionality

--- app.js
+++ app.js
@@ -31,7 +31,95 @@ addButton.addEventListener("click", openSidebar)
 closeButton.addEventListener("click", closeSidebar)
 submit.addEventListener("click", submitNewBook);
 
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("read-button")) {
+        changeCardRead(e);
+    }
+    if (e.target.classList.contains("remove-button")) {
+        const bookCard = e.target.closest('.book-card');
+        const id = bookCard.querySelector('p');
+        removeBookFromArray(id.innerText);
+        removeBookFromUI(bookCard);
+    }
 
+
+})
+
+function removeBookFromArray(id) {
+    myLibrary.splice(myLibrary.findIndex(item => item.id === id), 1);
+    console.log(myLibrary);
+}
+
+function removeBookFromUI(bookCard) {
+    bookCard.remove();
+}
+
+
+//**** Book Card ****//
+function showBooks() {
+
+    myLibrary.forEach((item) => {
+
+        createBookCard(item.title, item.author, item.pages, item.read, item.id)
+    });
+
+}
+
+function clearBookCardDisplays() {
+    while (cards.firstChild) {
+        cards.removeChild(cards.firstChild);
+    }
+}
+
+function createBookCard(title, author, pages, read, id) {
+    const card = document.createElement("div");
+    card.classList.add("book-card");
+    cards.appendChild(card);
+
+    const cardID = document.createElement("p");
+    cardID.classList.add("card-id");
+    cardID.innerText = id;
+    card.appendChild(cardID);
+
+    const cardTitle = document.createElement("div");
+    cardTitle.innerText = title;
+    cardTitle.classList.add("book-card-textfield");
+    card.appendChild(cardTitle);
+
+    const cardAuthor = document.createElement("div");
+    cardAuthor.innerText = author;
+    cardAuthor.classList.add("book-card-textfield");
+    card.appendChild(cardAuthor);
+
+
+
+    const cardPages = document.createElement("div");
+    cardPages.innerText = pages;
+    cardPages.classList.add("book-card-textfield");
+    card.appendChild(cardPages);
+
+    const buttonContainer = document.createElement("div");
+    card.appendChild(buttonContainer);
+
+    const cardRead = document.createElement("button");
+    cardRead.innerText = read;
+    buttonContainer.appendChild(cardRead);
+
+    if (cardRead.innerText === "Read") {
+
+        cardRead.classList.add("btn-success");
+    }
+    else cardRead.classList.add("btn-danger");
+    cardRead.classList.add("read-button");
+
+
+
+    const cardRemove = document.createElement("button");
+    cardRemove.innerText = "×";
+    cardRemove.classList.add("remove-button");
+    card.appendChild(cardRemove)
+
+}
 
 function submitNewBook(event) {
     const title = document.querySelector("#newTitle").value;
@@ -43,65 +131,6 @@ function submitNewBook(event) {
     addBookToArr(title, author, page, read);
     showBooks();
     event.preventDefault();
-}
-
-
-//**** Book Card ****//
-function showBooks() {
-
-    myLibrary.forEach((item) => {
-
-        createBookCard(item.title, item.author, item.pages, item.read)
-    });
-
-}
-
-function clearBookCardDisplays() {
-    while (cards.firstChild) {
-        cards.removeChild(cards.firstChild);
-    }
-}
-
-function createBookCard(title, author, pages, read) {
-    const card = document.createElement("div");
-    card.classList.add("book-card");
-    cards.appendChild(card);
-
-    const cardTitle = document.createElement("div");
-    cardTitle.innerText = title;
-    card.appendChild(cardTitle);
-
-    const cardAuthor = document.createElement("div");
-    cardAuthor.innerText = author;
-    card.appendChild(cardAuthor);
-
-
-
-    const cardPages = document.createElement("div");
-    cardPages.innerText = pages;
-    card.appendChild(cardPages);
-
-    const buttonContainer = document.createElement("div");
-    card.appendChild(buttonContainer);
-
-    const cardRead = document.createElement("button");
-    cardRead.innerText = read;
-    buttonContainer.appendChild(cardRead);
-    if (cardRead.innerText === "Read") {
-
-        cardRead.classList.add("btn-success");
-    }
-    else cardRead.classList.add("btn-danger");
-
-    cardRead.addEventListener("click", changeCardRead)
-
-
-
-    const cardRemove = document.createElement("button");
-    cardRemove.innerText = "Remove";
-    cardRemove.classList.add("btn-danger");
-    buttonContainer.appendChild(cardRemove)
-
 }
 
 function changeCardRead(event) {
@@ -131,3 +160,7 @@ function closeSidebar() {
     sidebar.style.width = "0";
     main.style.marginLeft = "0";
 }
+
+
+addBookToArr("title", "author", 3, "read");
+showBooks();
